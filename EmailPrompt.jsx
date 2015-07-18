15ae9fc7e28e60cbcd6374c01f4c32c7bf5b8821
mgbennet <mgbennet@gmail.com>
2015-07-18 14:15:51 -0700

    Added welcome on index.html, made email prompt

--- EmailPrompt.jsx
+++ EmailPrompt.jsx
@@ -1,5 +1,11 @@
 var EmailPrompt = React.createClass({
   render: function () {
-    return (<h1>Email Prompt!</h1>);
+    return (
+			<div id="emailPromt">
+				<h2>What's your email?</h2>
+				<input id="emailInput" type="email"/>
+				<input id="emailSubmit" type="submit" value="Submit email"/>
+			</div>
+		);
   }
 });
