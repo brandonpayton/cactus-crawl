83d3a5cca26dff51e9f8df33467bf0edf1d69158
Anthony Rivas <roboticsboy09@gmail.com>
2015-07-18 13:57:43 -0700

    Initial PHP commits
    Adding conn.php, getPubs.php, and getVotes.php

---
+++ php/conn.php
@@ -0,0 +1,10 @@
+<?PHP
+//Database Login Information for the Cactus Crawl Web App
+//Copyright Oblique Systems 2015 All Rights Reserved
+//Authors: Anthony Rivas
+date_default_timezone_set('America/New_York');
+$username = 'myobliqu_admin'
+$password = 'whatever';
+$host = 'localhost';
+$dbname = 'myobliqu_cactushack';
+?>
