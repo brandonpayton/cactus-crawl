b382cac030221ff7236cc4559aed9ef5f2529630
Brandon Payton <brandon@happycode.net>
2015-07-18 15:16:20 -0700

    Add request for voting list data (stubbed)

--- VotingList.jsx
+++ VotingList.jsx
@@ -1,5 +1,20 @@
 var VotingList = React.createClass({
+  getInitialState: function () {
+    return {
+      pubList: []
+    };
+  },
+
+  componentWillMount: function () {
+    var self = this;
+    $.get("stub/pubList.json").then(function (pubList) {
+      self.setState(_.assign({}, { pubList: pubList }));
+    });
+  },
+
   render: function () {
-    return (<h1>Pub List</h1>);
+    var state = this.state;
+    var pubList = state.pubList;
+    return (<h1>Pub List {pubList.length}</h1>);
   }
 });
