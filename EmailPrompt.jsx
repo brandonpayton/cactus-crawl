7289bdd8e2b5e75cc5d4647d86309f22ad75cb5e
mgbennet <mgbennet@gmail.com>
2015-07-18 15:23:03 -0700

    Added publistings to voting list

--- EmailPrompt.jsx
+++ EmailPrompt.jsx
@@ -1,11 +1,11 @@
 var EmailPrompt = React.createClass({
   render: function () {
     return (
-			<div id="emailPromt">
-				<h2>What's your email?</h2>
-				<input id="emailInput" type="email"/>
-				<input id="emailSubmit" type="submit" value="Submit email"/>
-			</div>
-		);
+      <div id="emailPromt">
+        <h2>What's your email?</h2>
+        <input id="emailInput" type="email"/>
+        <input id="emailSubmit" type="submit" value="Submit email"/>
+      </div>
+    );
   }
 });
--- VotingList.jsx
+++ VotingList.jsx
@@ -15,6 +15,32 @@ var VotingList = React.createClass({
   render: function () {
     var state = this.state;
     var pubList = state.pubList;
-    return (<h1>Pub List {pubList.length}</h1>);
+    var pubs = pubList.map(function(pub) {
+      return (
+        <PubListing name={pub.name} description={pub.description} />
+      )
+    })
+    return (
+      <div id="votingList">
+        <h1>Pubs to vote on</h1>
+        {pubs}
+      </div>
+    );
+  }
+});
+
+var PubListing = React.createClass({
+  render: function () {
+    return (
+      <div className="pubListing">
+        <h3 className="pubListingName">
+          {this.props.name}
+        </h3>
+        <p className="pubDescription">
+          {this.props.description}
+        </p>
+        <input type="submit" className="pubVote" value="Vote" />
+      </div>
+    )
   }
 });
